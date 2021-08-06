269c2f5b37657f9ccc001c618c84c8aa3eb54b2f
thash-ebm <thash-ebm@users.noreply.github.com>
2021-08-06 04:20:37 +0000

    deploy: c852e6bcb457acaf59e3c098f5eefe106957babe

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/fastai_page_test/assets/js/search-data.json', true);
+    request.open('GET', '/My_blog/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
